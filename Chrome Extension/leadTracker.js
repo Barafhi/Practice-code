bdd6c825c8b5fb610258325ccfdcc1894c375e6b
Thendo Ramurafhi <thendoramurafhi08@outlook.com>
2024-02-10 13:12:39 +0200

    getting input element to a varible using "let"

--- Chrome Extension/leadTracker.js
+++ Chrome Extension/leadTracker.js
@@ -1,6 +1,9 @@
 
 let inputBtn = document.getElementById("input-btn")
+let myLeads = []
+let inputEl = document.getElementById("input-el")
 
 inputBtn.addEventListener("click", function(){
-    console.log("button clicked from addEventlistener")
+   // console.log("button clicked from addEventlistener")
+
 })

6bdffcd7a838f87c936723463dbba0df6f827e1c
Thendo Ramurafhi <thendoramurafhi08@outlook.com>
2024-02-10 13:51:14 +0200

    using innerHTML to list the leads inside the js

--- Chrome Extension/leadTracker.js
+++ Chrome Extension/leadTracker.js
@@ -3,6 +3,7 @@ const inputBtn = document.getElementById("input-btn")
 let myLeads = []
 // const make sure that the developer can know which variable can be reassigned
 const inputEl = document.getElementById("input-el")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function(){
    // console.log("button clicked from addEventlistener")
@@ -10,3 +11,7 @@ inputBtn.addEventListener("click", function(){
    myLeads.push(inputEl.value)
 
 })
+
+for(let i = 0; i < myLeads.length; i++){
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}

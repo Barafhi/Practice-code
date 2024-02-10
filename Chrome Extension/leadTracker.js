2594d4cd1de8f820e46e21fe2f7ffa295863cf2e
Thendo Ramurafhi <thendoramurafhi08@outlook.com>
2024-02-10 13:14:15 +0200

    changing "let" to "const" because const cannot be reassigned

--- Chrome Extension/leadTracker.js
+++ Chrome Extension/leadTracker.js
@@ -1,7 +1,7 @@
 
-let inputBtn = document.getElementById("input-btn")
+const inputBtn = document.getElementById("input-btn")
 let myLeads = []
-let inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 
 inputBtn.addEventListener("click", function(){
    // console.log("button clicked from addEventlistener")

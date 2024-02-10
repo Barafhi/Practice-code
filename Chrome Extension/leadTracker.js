1fff592ac6685602b83eb5742d34578f23d4683e
Thendo Ramurafhi <thendoramurafhi08@outlook.com>
2024-02-10 13:28:04 +0200

    this is one of the method that we can use to get the value from the input field js

--- Chrome Extension/leadTracker.js
+++ Chrome Extension/leadTracker.js
@@ -1,9 +1,12 @@
 
 const inputBtn = document.getElementById("input-btn")
 let myLeads = []
+// const make sure that the developer can know which variable can be reassigned
 const inputEl = document.getElementById("input-el")
 
 inputBtn.addEventListener("click", function(){
    // console.log("button clicked from addEventlistener")
+   const input = inputEl
+   myLeads.push(input)
 
 })
